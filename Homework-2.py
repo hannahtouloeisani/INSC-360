3707aed625a1b8a4020c858b01db05c69edc6508
hannahtouloeisani <60446390+hannahtouloeisani@users.noreply.github.com>
2020-02-09 14:32:42 -0500

    Add files via upload

---
+++ Homework-2.py
@@ -0,0 +1,19 @@
+# Author: Hannah Touloeisani
+# Date: 2-9-20
+# Description: Homework 2
+
+string_1 = "This is a test string." #(2)(c)
+string_2 = "This is also a test string."
+string_3 = "Knock Knock! \nWho's there? \nOrange! \nOrange who? \nOrange you glad I didn't say \"banana\"?!"
+number_string = "1234"
+print(string_1) #(2)(d)
+print(string_2)
+print(string_3)
+print(number_string)
+joined_string = string_1 + " " + string_2 #(2)(e)
+string_3 = string_3.replace('banana', 'grapes')
+print(string_3) #(2)(f)
+new_numbers = int(number_string) #(2)(g)
+print(type(new_numbers))
+
+
